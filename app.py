5e8d6458266b2c63d654d174b6e688f3e963f058
ahmadmustafaanis <ahmadanis5050@gmail.com>
2021-03-19 07:36:22 +0500

    FIRST TRY

---
+++ app.py
@@ -0,0 +1,60 @@
+import numpy as np
+from fastapi import FastAPI
+import pandas as pd 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from model import preProcess_data
+from pydantic import BaseModel
+import tensorflow as tf
+import re
+def preProcess_data(text):
+    
+    text = text.lower()
+    new_text = re.sub('[^a-zA-z0-9\s]','',text)
+    new_text = re.sub('rt', '', new_text)
+    return new_text
+
+app = FastAPI()
+
+data = pd.read_csv('archive/Sentiment.csv')
+tokenizer = Tokenizer(num_words=2000, split=' ')
+tokenizer.fit_on_texts(data['text'].values)
+
+
+
+def my_pipeline(text):
+  text_new = preProcess_data(text)
+  X = tokenizer.texts_to_sequences(pd.Series(text_new).values)
+  X = pad_sequences(X, maxlen=28)
+  return X
+
+
+class inputToModel(BaseModel):
+    text:str
+
+
+@app.get('/')
+def basic_view():
+    return {"WELCOME": "GO TO /docs route or send post request to /predict "}
+
+
+@app.post('/predict')
+def predict(inp: inputToModel):
+    clean_text = my_pipeline(inp.text)
+    loaded_model = tf.keras.models.load_model('sentiment.h5')
+    predictions = loaded_model.predict(clean_text)
+    sentiment = int(np.argmax(predictions))
+    probability = max(predictions.tolist()[0])
+    print(sentiment)
+    if sentiment==0:
+        t_sentiment = 'negative'
+    elif sentiment==1:
+        t_sentiment = 'neutral'
+    elif sentiment==2:
+        t_sentiment='postive'
+    
+    return {
+        "ACTUALL SENTENCE": inp.text,
+        "PREDICTED SENTIMENT": t_sentiment,
+        "Probability": probability
+    }
